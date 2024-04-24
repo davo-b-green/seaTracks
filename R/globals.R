57cf04950fbc64698f7348a0a4307155671855f2
David Green <45384906+davo-b-green@users.noreply.github.com>
2024-04-24 10:39:55 +1000

    Correcting merging of location and additional data in dive and ctd datasets, for track processing

--- R/globals.R
+++ R/globals.R
@@ -1,2 +1,2 @@
 utils::globalVariables(c("ref", "end_date", "sequential",  'd_date', 'de_date', "s_date", "e_date", "end_date", "id",
-                         "lq", "lon", "lat", "last", "fisrt", "tgap", "tgap_dur", "date1", "date2", ".", "time_gap_ok", "tdif"))
+                         "lq", "lon", "lat", "last", "fisrt", "tgap", "tgap_dur", "date1", "date2", ".", "time_gap_ok", "tdif", "g", "logit_g", "logit_g.se")) # added 24-04-2024: g logit_g logit_g.se
