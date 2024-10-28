247fd9986518e2c36ed6181eef054e85dc325afb
David Green <davo.b.green@gmail.com>
2024-10-28 14:37:47 +1100

    updating group_split to group_by(id) %>% group_split() according to new dplyr instructions

--- R/process_tracks.R
+++ R/process_tracks.R
@@ -365,7 +365,8 @@ process_tracks <- function(in_loc = "./compiled_raw_datasets/loc_all_raw_pre-qc.
       if(nrow(tgaps) > 0){
         fit_all_sub <- fit_all %>%
           filter(id %in% unique(tgaps$id)) %>%
-          group_split(id) %>%
+          group_by(id) %>%
+          group_split() %>%
           lapply(., function(x){
             sub.gap = filter(tgaps, id %in% x$id[1])
             x = x %>%
